95f8582b3095559a281cb683b6e15b5b843eed93
Rodrigo Godoy MacPro <rodrigocgodoy@hotmail.com>
2021-06-27 00:34:19 -0300

    Remove dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,22 @@ module.exports = {
   purge: {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   },
-  darkMode: 'class',
-  variants: {
-    typography: ['dark'],
-  },
-	theme: {
-    typography: (theme) => ({}),
-    extend: {
-      typography: (theme) => ({
-        dark: {
-          css: {
-            color: 'white',
-          },
-        },
-      }),
-    },
-  },
-  plugins: [require('@tailwindcss/typography')]
+  darkMode: false,
+  // darkMode: 'class',
+  // variants: {
+  //   typography: ['dark'],
+  // },
+	// theme: {
+  //   typography: (theme) => ({}),
+  //   extend: {
+  //     typography: (theme) => ({
+  //       dark: {
+  //         css: {
+  //           color: 'white',
+  //         },
+  //       },
+  //     }),
+  //   },
+  // },
+  // plugins: [require('@tailwindcss/typography')]
 }
