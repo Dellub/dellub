e58355bf76f3762c4b9ac69a3f8860e4946d2d58
Rodrigo Godoy MacPro <rodrigocgodoy@hotmail.com>
2021-06-27 02:36:36 -0300

    Added componente icon

---
+++ src/pages/components/Icon.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type ButtonProps = {
+	children: ReactNode;
+}
+
+const Button = ({ children }: ButtonProps) => {
+	return (
+		<span className="icon icon-default">
+			{children}
+		</span>
+	);
+};
+
+export default Button;
