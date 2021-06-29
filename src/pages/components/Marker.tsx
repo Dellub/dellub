575c8c90f480a20022a89d56340acd807b8cac4d
Rodrigo Godoy MacPro <rodrigocgodoy@hotmail.com>
2021-06-29 09:08:58 -0300

    Add component Marker

---
+++ src/pages/components/Marker.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Marker = () => {
+	return <Image src="/images/marker.png" alt="Marker title" width={36} height={36} />;
+};
+
+export default Marker;
