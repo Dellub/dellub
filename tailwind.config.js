f54b2b2906468fb817a999acb88269a57eec714a
Rodrigo Godoy MacPro <rodrigocgodoy@hotmail.com>
2021-06-29 09:58:40 -0300

    wip: config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,50 @@ module.exports = {
   },
 	theme: {
     typography: (theme) => ({}),
-    extend: {
-      typography: (theme) => ({
-        dark: {
-          css: {
-            color: 'white',
-          },
-        },
-      }),
-    },
+		screens: {
+			sm: '540px',
+			md: '720px',
+			lg: '960px',
+			xl: '1440px',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Heebo', 'sans-serif'],
+				serif: ['Heebo', 'serif'],
+			},
+			fontSize: {
+				text: {
+					'3xl': '2rem',
+					'4xl': '2.5rem',
+					'5xl': '3.5rem',
+					'8xl': '6.5rem',
+				}
+			},
+			lineHeight: {
+	
+			},
+			colors: {
+				gray: {
+					900: '#3B3F49',
+					800: '#474F63',
+					700: '#6E778D',
+					600: '#B3B9C9',
+					500: '#C6CAD6',
+					400: '#E2E5ED',
+				},
+				pink: {
+					900: '#E54181',
+				},
+				primary: '#4B74EB',
+			},
+			typography: (theme) => ({
+				dark: {
+					css: {
+						color: 'white',
+					},
+				},
+			}),
+		},
   },
   plugins: [require('@tailwindcss/typography')]
 }
