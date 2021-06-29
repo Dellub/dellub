86c2e27bcc90533a31112edc5180f4f5bb3ad631
Rodrigo Godoy MacPro <rodrigocgodoy@hotmail.com>
2021-06-29 09:09:23 -0300

    Add custom fonts Heebo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,15 +11,9 @@ export default class IntlDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link
-            rel="preload"
-            as="style"
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          /> */}
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+					<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body className="bg-gray-100 dark:bg-gray-800">
           <Main />
